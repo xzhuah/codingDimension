2313d62d1d92d5d19e22e99757e0ed015fd28ffb
xzhuah <xzhuah@connect.ust.hk>
2020-11-05 22:48:50 -0800

    update ignore

---
+++ xinyu/java/src/main/java/nodes/stockinfoNode/integration/KeyReader.java
@@ -0,0 +1,14 @@
+package nodes.stockinfoNode.integration;
+
+import common.io.file.PlaintextClient;
+
+/**
+ * Created by Xinyu Zhu on 2020/11/5, 22:46
+ * nodes.stockinfoNode.integration in codingDimensionTemplate
+ */
+public class KeyReader {
+    private static final String keyFile = "key.pass";
+    public static getKey() {
+
+    }
+}
